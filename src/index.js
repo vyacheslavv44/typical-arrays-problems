0c1f4619cd3c71e2f43c6c9145589f5c2dcb59f2
vyacheslavv44 <follow.trend7777@gmail.com>
2020-03-22 01:15:33 +0400

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) { 
+    return !(array && array.length) ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return !(array && array.length) ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return !(array && array.length) ? 0 : array.reduce((a, b) => a + b, 0) / array.length;
 }
